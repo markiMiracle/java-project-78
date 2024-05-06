86fbdbeb019bbe6a22fc2f0325309e38a04790d4
markiMiracle <someonewhosome@list.ru>
2024-05-06 13:19:40 +0300

    make lint

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -3,7 +3,6 @@ package hexlet.code;
 import org.junit.jupiter.api.Test;
 
 import static hexlet.code.App.hello;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
